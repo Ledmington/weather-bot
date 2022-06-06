344a411624e09caca362b4b9d52775533c4a9b24
Ledmington <ledmington.dev@gmail.com>
2022-06-06 10:30:57 +0200

    Minor improvements

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="weather-bot")
+location = geolocator.geocode("Riccione")
+print(location.address)
+print((location.latitude, location.longitude))
+print(location.raw)
+
+with open("weather-key.txt", "r") as f:
+    api_key = f.read()
+print(api_key)
